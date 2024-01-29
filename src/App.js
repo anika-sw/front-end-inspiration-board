8e536bb3e3626b4ea958aae9184b1cafdae38632
Anika Stephen Wilbur <anika.m.stephen@gmail>
2024-01-29 00:21:51 -0800

    Add backend url to heroku config vars

--- src/App.js
+++ src/App.js
@@ -5,238 +5,233 @@ import NewBoardForm from "./components/NewBoardForm";
 import Board from "./components/Board";
 import "./styles/App.css";
 
+const kBaseUrl = process.env.REACT_APP_BE_URL;
+
 function App() {
+	const [boardsData, setBoardsData] = useState([]);
+	const [cardsData, setCardsData] = useState([]);
+	const [selectedBoard, setSelectedBoard] = useState({
+		title: "",
+		owner: "",
+		board_id: null,
+	});
+	const [isBoardFormShowing, setIsBoardFormShowing] = useState(true);
 
-  const [boardsData, setBoardsData] = useState([]);
-  const [cardsData, setCardsData] = useState([]);
-  const [selectedBoard, setSelectedBoard] = useState({
-    title: "",
-    owner: "",
-    board_id: null,
-  });
-  const [isBoardFormShowing, setIsBoardFormShowing] = useState(true);
+	const selectBoard = (board) => {
+		setSelectedBoard(board);
+	};
 
-  const selectBoard = (board) => {
-    setSelectedBoard(board);
-  };
+	const resetSelectBoard = () => {
+		setSelectedBoard({
+			title: "",
+			owner: "",
+			board_id: null,
+		});
+	};
 
-  const resetSelectBoard = () => {
-    setSelectedBoard(
-      {
-        title: "",
-        owner: "",
-        board_id: null,
-      }
-    )
-  };
+	const toggleNewBoardForm = () => {
+		setIsBoardFormShowing(!isBoardFormShowing);
+	};
 
-  const toggleNewBoardForm = () => {
-    setIsBoardFormShowing(!isBoardFormShowing);
-  };  
+	useEffect(() => {
+		axios.get(`${kBaseUrl}/boards`, {}).then((response) => {
+			setBoardsData(response.data);
+		});
+	}, []);
 
-  useEffect(() => {
-    axios
-      .get("https://inspo-board-backend-haam-e0458211a663.herokuapp.com/boards", {})
-      .then((response) => {
-        setBoardsData(response.data);
-    });
-  }, []);
+	const getBoardCards = (board_id) => {
+		axios
+			.get(`${kBaseUrl}/boards/${board_id}/cards`)
+			.then((response) => {
+				setCardsData(response.data.cards);
+			})
+			.catch((error) => {
+				console.log("Error:", error);
+				alert("Unable to retrieve cards for this board");
+			});
+	};
 
-  const getBoardCards = (board_id) => {
-    axios
-      .get(
-        `https://inspo-board-backend-haam-e0458211a663.herokuapp.com/boards/${board_id}/cards`
-      )
-      .then((response) => {
-        setCardsData(response.data.cards);
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-        alert("Unable to retrieve cards for this board");
-    });
-  };
+	const boardsList = boardsData.map((board, index) => {
+		return (
+			<li key={index}>
+				<Board
+					board={board}
+					onSelect={selectBoard}
+					displayCards={getBoardCards}
+				></Board>
+			</li>
+		);
+	});
 
-  const boardsList = boardsData.map((board, index) => {
-    return (
-      <li key={index}>
-        <Board
-          board={board}
-          onSelect={selectBoard}
-          displayCards={getBoardCards}
-        ></Board>
-      </li>
-    );
-  });
+	const createNewBoard = (newBoard) => {
+		axios
+			.post(`${kBaseUrl}/boards`, newBoard)
+			.then((response) => {
+				console.log("Response:", response.data.boards);
+				const boards = [...boardsData];
+				boards.push(response.data.boards);
+				setBoardsData(boards);
+			})
+			.catch((error) => {
+				console.log("Error:", error);
+				alert("Error creating a new board");
+			});
+	};
 
-  const createNewBoard = (newBoard) => {
-    axios
-      .post(
-        "https://inspo-board-backend-haam-e0458211a663.herokuapp.com/boards", newBoard
-      )
-      .then((response) => {
-        console.log("Response:", response.data.boards);
-        const boards = [...boardsData];
-        boards.push(response.data.boards);
-        setBoardsData(boards);
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-        alert("Error creating a new board");
-    });
-  };
+	const deleteOneBoard = (board_id) => {
+		axios
+			.delete(`${kBaseUrl}/boards/${board_id}`)
+			.then((response) => {
+				const newBoardsData = boardsData.filter((deletedBoard) => {
+					return deletedBoard.board_id !== board_id;
+				});
+				setBoardsData(newBoardsData);
+				resetSelectBoard();
+			})
+			.catch((error) => {
+				console.log("Error:", error);
+				alert("Unable to delete this board");
+			});
+	};
 
-  const deleteOneBoard = (board_id) => {
-    axios
-      .delete(
-        `https://inspo-board-backend-haam-e0458211a663.herokuapp.com/boards/${board_id}`
-      )
-      .then((response) => {
-        const newBoardsData = boardsData.filter((deletedBoard) => {
-          return deletedBoard.board_id !== board_id;
-        });
-        setBoardsData(newBoardsData);
-        resetSelectBoard();
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-        alert("Unable to delete this board");
-    });
-  };
+	const deleteAllBoards = (boards) => {
+		axios
+			.delete(`${kBaseUrl}/boards`)
+			.then((response) => {
+				setBoardsData([]);
+				resetSelectBoard();
+			})
+			.catch((error) => {
+				console.log("Error:", error);
+				alert("Unable to delete all boards");
+			});
+	};
 
-  const deleteAllBoards = (boards) => {
-    axios
-    .delete(
-      "https://inspo-board-backend-haam-e0458211a663.herokuapp.com/boards"
-      )
-    .then((response) => {;
-      setBoardsData([])
-      resetSelectBoard();
-    })
-    .catch((error) => {
-      console.log("Error:", error);
-      alert("Unable to delete all boards");
-    });
-  };
+	const createNewCard = (message) => {
+		axios
+			.post(`${kBaseUrl}/boards/${selectedBoard.board_id}/cards`, {
+				message,
+			})
+			.then((response) => {
+				console.log("Response:", response.data.cards);
+				const cards = [...cardsData];
+				cards.push(response.data.cards);
+				setCardsData(cards);
+			})
+			.catch((error) => {
+				console.log("Error:", error);
+				alert("Couldn't create a new card.");
+			});
+	};
 
-  const createNewCard = (message) => {
-    axios
-      .post(
-        `https://inspo-board-backend-haam-e0458211a663.herokuapp.com/boards/${selectedBoard.board_id}/cards`,
-        { message }
-      )
-      .then((response) => {
-        console.log("Response:", response.data.cards);
-        const cards = [...cardsData];
-        cards.push(response.data.cards);
-        setCardsData(cards);
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-        alert("Couldn't create a new card.");
-    });
-  };
+	const deleteCard = (cardId) => {
+		axios
+			.delete(`${kBaseUrl}/cards/${cardId}`)
+			.then((response) => {
+				const newCardsData = cardsData.filter((deletedCard) => {
+					return deletedCard.card_id !== cardId;
+				});
+				setCardsData(newCardsData);
+			})
+			.catch((error) => {
+				console.log("Error:", error);
+				alert("Unable to delete the selected card");
+			});
+	};
 
-  const deleteCard = (cardId) => {
-    axios
-      .delete(
-        `https://inspo-board-backend-haam-e0458211a663.herokuapp.com/cards/${cardId}`
-      )
-      .then((response) => {
-        const newCardsData = cardsData.filter((deletedCard) => {
-          return deletedCard.card_id !== cardId;
-        });
-        setCardsData(newCardsData);
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-        alert("Unable to delete the selected card");
-    });
-  };
+	const updateLikes = async (cardId) => {
+		try {
+			await axios.patch(
+				`${kBaseUrl}/boards/${selectedBoard.board_id}/cards/${cardId}`
+			);
+			onCardLikes(cardId);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-  const updateLikes = async (cardId) => {
-    try {
-      const res = await axios.patch(`https://inspo-board-backend-haam-e0458211a663.herokuapp.com/boards/${selectedBoard.board_id}/cards/${cardId}`);
-      onCardLikes(cardId);
-    } catch(err) {
-      console.error(err);
-    }
-  }
+	const onCardLikes = (cardId) => {
+		setCardsData(
+			cardsData.map((card) => {
+				if (card.card_id === cardId) {
+					return { ...card, likes: card.likes + 1 };
+				} else {
+					return card;
+				}
+			})
+		);
+	};
 
-  const onCardLikes = (cardId) => {
-    setCardsData(cardsData.map(card => {
-      if(card.card_id === cardId) {
-          return {...card, likes: card.likes + 1}
-      } else {
-          return card;
-      }
-  }));
-  }
-
-  return (
-    <main className="mainContainer">
-      <section>
-        <h1>Inspiration Board</h1>
-      </section>
-      <section>
-        <section className="upperGrid">
-          <section>
-            <h2 className="noBottomMargin">List of Boards</h2>
-            <div className="deleteBoards"
-              onClick={(event) => deleteAllBoards(boardsData)}>
-              Delete All Boards
-            </div>
-            <p>Select a board to view from the list below</p>
-            <ul className="listOfBoards">{boardsList}</ul>
-          </section>
-          <section>
-            <h2 className="noBottomMargin">Now Viewing:</h2>
-            <div className="deleteBoards"
-              onClick={(event) => deleteOneBoard(selectedBoard.board_id)}>
-              {selectedBoard.board_id 
-                ? "Delete This Board"
-                : ""}
-            </div>
-            <p className="selectedBoard">
-              {selectedBoard.board_id &&
-              `${selectedBoard.title} by ${selectedBoard.owner}`}
-            </p>
-          </section>
-          <section>
-            <h2>Need a New Board?</h2>
-            <p>Enter details below and click Create</p>
-              {isBoardFormShowing && (
-                <NewBoardForm
-                addNewBoard={createNewBoard}
-                ></NewBoardForm>
-              )}
-            <br />
-            <div
-              onClick={toggleNewBoardForm}
-              className="toggleNewBoardForm">
-              {isBoardFormShowing
-                ? "Hide New Board Form"
-                : "Show New Board Form"}
-            </div>
-          </section>
-        </section>
-        {selectedBoard.board_id && (
-          <CardsList 
-            board={selectedBoard}
-            onUpdateLikes={updateLikes}
-            onDeleteCard={deleteCard}
-            addNewCard={createNewCard}
-            cards={cardsData}
-          ></CardsList>
-        )}
-      </section>
-      <footer>
-        <h3>
-          © 2023 Inspiration Board | Hoku, Alaere, Anika, & Mia | C18
-          Snow Leopards
-        </h3>
-      </footer>
-    </main>
-  );
-};
+	return (
+		<main className="mainContainer">
+			<section>
+				<h1>Inspiration Board</h1>
+			</section>
+			<section>
+				<section className="upperGrid">
+					<section>
+						<h2 className="noBottomMargin">List of Boards</h2>
+						<div
+							className="deleteBoards"
+							onClick={(event) => deleteAllBoards(boardsData)}
+						>
+							Delete All Boards
+						</div>
+						<p>Select a board to view from the list below</p>
+						<ul className="listOfBoards">{boardsList}</ul>
+					</section>
+					<section>
+						<h2 className="noBottomMargin">Now Viewing:</h2>
+						<div
+							className="deleteBoards"
+							onClick={(event) =>
+								deleteOneBoard(selectedBoard.board_id)
+							}
+						>
+							{selectedBoard.board_id ? "Delete This Board" : ""}
+						</div>
+						<p className="selectedBoard">
+							{selectedBoard.board_id &&
+								`${selectedBoard.title} by ${selectedBoard.owner}`}
+						</p>
+					</section>
+					<section>
+						<h2>Need a New Board?</h2>
+						<p>Enter details below and click Create</p>
+						{isBoardFormShowing && (
+							<NewBoardForm
+								addNewBoard={createNewBoard}
+							></NewBoardForm>
+						)}
+						<br />
+						<div
+							onClick={toggleNewBoardForm}
+							className="toggleNewBoardForm"
+						>
+							{isBoardFormShowing
+								? "Hide New Board Form"
+								: "Show New Board Form"}
+						</div>
+					</section>
+				</section>
+				{selectedBoard.board_id && (
+					<CardsList
+						board={selectedBoard}
+						onUpdateLikes={updateLikes}
+						onDeleteCard={deleteCard}
+						addNewCard={createNewCard}
+						cards={cardsData}
+					></CardsList>
+				)}
+			</section>
+			<footer>
+				<h3>
+					© 2023 Inspiration Board | Hoku, Alaere, Anika, & Mia | C18
+					Snow Leopards
+				</h3>
+			</footer>
+		</main>
+	);
+}
 
 export default App;
